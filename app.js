655bd844d0e9ae92515ee6934a5c40c89b402562
unknown <jannathsyeda7@gmail.com>
2021-03-03 17:04:58 +0600

    display recipies from searching by item name

--- app.js
+++ app.js
@@ -1,48 +1,27 @@
-const searchMeals=search=>{
-    const inputField=document.getElementById("search-input").value;
-    // console.log(inputField);
-    const submitResult=document.getElementById("result");
-    // submitResult.innerHTML='';
-    const url=`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputField}`
-    fetch(url)
-    // .then(respon=>respon.json())
-    //  .then(data=>console.log(data))
+const searchMeals=()=>{
+    const InputMeal=document.getElementById('search-input').value;
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${InputMeal}`)
+    .then(res =>res.json())
+    // .then(data =>{
+    //     console.log(data.meals);
+    // })
+     .then(data =>displaymeals(data.meals))
+    // .catch(error =>('no serach result'));
+}
 
-    .then(data=>displaySearch(data.meals))
-    .catch(error=>{
-        const errordetails=document.getElementById('mealDivs');
-        const errorMessage=`<h1 id="errorMessage"> The ${inputField} you search is not found</h1>`
-        errorMessage.innerHTML=errorMessage;
-        const submitResult=document.getElementById("result");
-        // submitResult.innerHTML='';
-        // document.getElementById("submitResult").value='';
+const displaymeals =meals=>{
+    const mealDivsContainer=document.getElementById('mealDivsContainer');
+    mealDivsContainer.innerHTML= '';
+    meals.forEach(meal => {
+
+        const mealDiv=document.createElement('div');
+        mealDiv.className ="meal-card";
+        mealDiv.innerHTML= `
+        <img class="card-img-top" src="${meal.strMealThumb}">
+        <h5 class="card-title text-center">${meal.strMeal}</h5>
+`;
+
+mealDivsContainer.appendChild(mealDiv);
+
     })
-
-    const displaySearch=resdatas=>{
-        // const resdatasArray= resdatas.resdata
-        const submitResult=document.getElementById("result");
-        resdatas.ForEach(resdata=>{
-            const makeDiv=document.getElementById("div");
-            makeDiv.className="CreatedDivMeal";
-            const makeDivInfo=`
-            <img  src="${resdata.strMealThumb}">
-            <a  href=#>${resdata.strMeal}</a>
-
-
-            `;
-            // onclick="displayOneMealDetail(${resdata.idMeal})"
-            // onclick="displayOneMealDetail(${resdata.idMeal})"
-            makeDiv.innerHTML=makeDivInfo;
-            submitResult.appendChild(makeDiv);
-            const ErrorMessage=document.getElementById('errorMessage');
-            if(ErrorMessage !=null){
-                document.getElementById('errorMessage').innerText="";
-            }
-            document.getElementById('search-input').value="";
-
-
-            
-        });
-    }
-    
 }
